c770907e8fd1386f43c124937b8dbaed824c86ca
kevin-bonnot <bonnot.kevin@gmail.com>
2021-10-20 14:32:01 +0200

    fix ex 4

--- day-1/exercice-4.js
+++ day-1/exercice-4.js
@@ -1,7 +1,12 @@
 export const my_size_alpha = (str) => {
+    if (typeof str !== 'string') {
+        return 0;
+    }
     let len = 0;
     while ((typeof str[len]) !== 'undefined') {
         len++;
     }
     return len;
 }
+
+console.log(my_size_alpha(123))
