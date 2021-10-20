573a0c07b2b1f2360011d728e06415555d254ef8
kevin-bonnot <bonnot.kevin@gmail.com>
2021-10-20 16:03:18 +0200

    d2 ex5

--- day-2/exercice-5.js
+++ day-2/exercice-5.js
@@ -1,13 +1,15 @@
 export const my_display_combv2 = () => {
     const tab = [];
+    let index = 0;
     for (let i = 0; i < 100; i++) {
         for (let j = 0; j < 100; j++) {
             for (let k = 0; k < 100; k++) {
-                tab[j] = i + ' ' + j + ' ' + k;
+                if (j > i && k > j) {
+                    tab[index] = i + ' ' + j + ' ' + k;
+                    index++;
+                }
             }
         }
     }
     return tab;
 }
-
-console.log(my_display_combv2().length)
